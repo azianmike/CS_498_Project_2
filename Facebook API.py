325528ad94118c187ac63afe003e4ec57d7ac5eb
michaelluo <michaeluo@gmail.com>
2014-02-28 16:11:17 -0600

    Testing with fields

--- Facebook API.py
+++ Facebook API.py
@@ -3,7 +3,11 @@ __author__ = 'michaelluo'
 from urllib2 import urlopen
 import json
 
-testOpen = urlopen('https://graph.facebook.com/563700600')
+testOpen = urlopen('https://graph.facebook.com/563700600')  #my own ID
 testOpenJSON = json.load(testOpen)
 print testOpen
 print testOpenJSON
+
+testOpenWithFields = urlopen('https://graph.facebook.com/563700600?fields=id,name')
+testOpenJSONFields = json.load(testOpenWithFields)
+print testOpenJSONFields
