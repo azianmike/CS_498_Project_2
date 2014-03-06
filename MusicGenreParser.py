777ba67346397feed711138c9990ec70af73a1c3
michaelluo <michaeluo@gmail.com>
2014-03-06 12:33:05 -0600

    Added code to get person's likes

--- MusicGenreParser.py
+++ MusicGenreParser.py
@@ -7,6 +7,7 @@ openData = open('jay-friends-music.txt')
 allData = json.load(openData)['data']
 
 musicOver50 = []
+musicOver50List = []
 
 #Getting data from txt file and putting into one giant list
 for person in allData:
@@ -15,6 +16,7 @@ for person in allData:
     if len(personMusic) >= 50:
         personMusic = sorted(personMusic)
         musicOver50 = musicOver50 + personMusic
+        musicOver50List.append(personMusic)
         # for x in personMusic:
         #     if artistGenre.has_key(x.lower()) is False:
         #         print x.lower()
@@ -44,4 +46,35 @@ count = int(jayBensal['Other'])
 jayBensal['Other'] = '0'
 notCount = int(jayBensal['Not classified'])
 jayBensal['Not classified'] = count+notCount
-print jayBensal
+#print jayBensal
+
+#classifying each person
+print musicOver50List
+personLikesList = []
+#goes through every person
+for person in musicOver50List:
+    personLikes = {
+        'Hip Hop':'0',
+        'Rock':'0',
+        'Pop':'0',
+        'Electronic':'0',
+        'Alternative':'0',
+        'Country':'0',
+        'Other':'0',
+        'Not classified':'0'
+    }
+    #goes through every artist in persons like
+    for artist in person:
+        if artistGenre.has_key(artist.lower()):
+            genre = artistGenre[artist.lower()]
+            count = int(personLikes[genre])+1
+            personLikes[genre] = count
+        else:
+            count = int(personLikes['Not classified']) + 1
+            personLikes['Not classified'] = count
+
+    personLikesList.append(personLikes)
+
+#prints out
+for x in personLikesList:
+    print x
