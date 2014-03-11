070a6e4cb7290325a7d8cfe40e5f4ba95d6ca34f
michaelluo <michaeluo@gmail.com>
2014-03-10 20:50:44 -0500

    Adding percents JSON

--- MusicGenreParser.py
+++ MusicGenreParser.py
@@ -51,34 +51,84 @@ jayBensal['Not classified'] = count+notCount
 
 #classifying each person
 personLikesList = {}
+personLikesPercentList = {}
 count = 1
+
+countLoop=0
 #goes through every person
 for person in musicOver50List.keys():
     personLikes = {
-        'Hip Hop':'0',
-        'Rock':'0',
-        'Pop':'0',
-        'Electronic':'0',
-        'Alternative':'0',
-        'Country':'0',
-        'Other':'0',
-        'Not classified':'0'
+        'Hip Hop':0.0,
+        'Rock':0.0,
+        'Pop':0.0,
+        'Electronic':0.0,
+        'Alternative':0.0,
+        'Country':0.0,
+        'Other':0.0,
+    }
+
+    personLikesPercents = {
+        'Not classified':0.0,
+        'Hip Hop/Pop':0.0,
+        'Hip Hop/Alternative':0.0,
+        'Hip Hop/Electronic':0.0,
+        'Rock/Pop':0.0,
+        'Rock/Alternative':0.0,
+        'Rock/Electronic':0.0,
+        'Country/Pop':0.0,
+        'Country/Alternative':0.0,
+        'Country/Electronic':0.0
     }
     #goes through every artist in persons like
     for artist in musicOver50List[person]:
         if artistGenre.has_key(artist.lower()):
             genre = artistGenre[artist.lower()]
-            count = int(personLikes[genre])+1
+            count = personLikes[genre]+1
             personLikes[genre] = count
         else:
-            count = int(personLikes['Not classified']) + 1
-            personLikes['Not classified'] = count
+            count+=1 #do nothing
 
-    personLikesList[count]=personLikes
+    hiphopPop = personLikes['Hip Hop'] + personLikes['Pop']
+    hiphopAlternative = personLikes['Hip Hop'] + personLikes['Alternative']
+    hiphopElectronic = personLikes['Hip Hop'] + personLikes['Electronic']
+    rockPop = personLikes['Rock'] + personLikes['Pop']
+    rockAlternative = personLikes['Rock'] + personLikes['Alternative']
+    rockElectronic = personLikes['Rock'] + personLikes['Electronic']
+    countryPop = personLikes['Country'] + personLikes['Pop']
+    countryAlternative = personLikes['Country'] + personLikes['Alternative']
+    countryElectronic = personLikes['Country'] + personLikes['Electronic']
+
+    if hiphopPop != 0:
+        personLikesPercents['Hip Hop/Pop'] = personLikes['Hip Hop']/hiphopPop
+    if hiphopAlternative != 0:
+        personLikesPercents['Hip Hop/Alternative'] = personLikes['Hip Hop']/hiphopAlternative
+    if hiphopElectronic != 0:
+        personLikesPercents['Hip Hop/Electronic'] = personLikes['Hip Hop']/hiphopElectronic
+    if rockPop != 0:
+        personLikesPercents['Rock/Pop'] = personLikes['Rock']/rockPop
+    if rockAlternative != 0:
+        personLikesPercents['Rock/Alternative'] = personLikes['Rock']/rockAlternative
+    if rockElectronic != 0:
+        personLikesPercents['Rock/Electronic'] = personLikes['Rock']/rockElectronic
+    if countryPop != 0:
+        personLikesPercents['Country/Pop'] = personLikes['Country']/countryPop
+    if countryAlternative != 0:
+        personLikesPercents['Country/Alternative'] = personLikes['Country']/countryAlternative
+    if countryElectronic != 0:
+        personLikesPercents['Country/Electronic'] = personLikes['Country']/countryElectronic
+
+
+    personLikesList[person]=personLikes
+    personLikesPercentList[person]=personLikesPercents
     count+=1
+    countLoop+=1
 
-
+print countLoop
 #print personLikesList
-
+print len(personLikesList)
+print len(personLikesPercentList)
 with open('JayBensalFriendData.txt', 'w') as outfile:
     json.dump(personLikesList, outfile, indent=4)
+
+with open('JayBensalFriendDataPercents.txt', 'w') as outfile:
+    json.dump(personLikesPercentList, outfile, indent=4)
